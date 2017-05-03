760e76887365206c784b504981517329a03cad09
danielseaton <daniel.seaton87@gmail.com>
2017-05-03 22:16:53 +0100

    Corrected code

--- split_and_sum_TE_counts.py
+++ split_and_sum_TE_counts.py
@@ -54,20 +54,27 @@ assert(len(footer_ids)<15)
 eDF.loc[gene_ids+footer_ids,:].to_csv(args.output_prefix+'_gene_id_counts.tsv',sep='\t')
 
 #Cut down matrix to just TE elements
-TE_ids = TE_df['transcript_id'].to_list()
+TE_ids = list(set(TE_df['transcript_id'])&set(eDF.index))
 eDF = eDF.loc[TE_ids+footer_ids,:]
 
 #Output to file
 output_filename = '{}_TE{}_counts.tsv'.format(args.output_prefix,'transcript_id')
 eDF.to_csv(output_filename,sep='\t')
 
-levels = ['gene_id','family_id','class_id'] #MUST be in increasing level in heirarchy
+levels = ['gene_id','family_id','class_id'] #Must be in increasing level in heirarchy
 previous_level = 'transcript_id' #start with the most basic element
 for level in levels:
-    output_eDF = pd.DataFrame(0,columns=eDF.columns,index=IDs)
+    # Make an index for the TE dataframe at this level of description
+    new_TE_index = list(TE_df[level]) + footer_ids
+    output_eDF = pd.DataFrame(0,columns=eDF.columns,index=new_TE_index)
+    output_eDF.loc[footer_ids,:] = eDF.loc[footer_ids,:]
+
+    # Each row from the lower level gets added to one row of the next level up
     for TE_id in eDF.index:
-        TE_id_to_add_to = TE_df[TE_df[previous_level]==TE_id][level]
-        output_eDF.loc[TE_id_to_add_to,:] = eDF.loc[TE_id,:]
+        TE_df_selection = TE_df[previous_level]==TE_id
+        assert(sum(TE_df_selection)==1)
+        TE_id_to_add_to = TE_df.loc[TE_df_selection,level]
+        output_eDF.loc[TE_id_to_add_to,:].add(eDF.loc[TE_id,:],axis='columns')
 
     output_filename = '{}_TE{}_counts.tsv'.format(args.output_prefix,level)
     output_eDF.to_csv(output_filename,sep='\t')
@@ -75,3 +82,4 @@ for level in levels:
     # This reduces the number of summations required
     eDF = output_eDF
     previous_level = level
+
