29e6fc71a76c9df2290ab97d5c1e32c9c1e3f337
danielseaton <daniel.seaton87@gmail.com>
2017-09-05 12:39:41 +0100

    PEP8 compliance

--- bin/split_and_sum_TE_counts.py
+++ bin/split_and_sum_TE_counts.py
@@ -29,27 +29,27 @@ parser.add_argument("-m",
 args = parser.parse_args()
 
 
-#Set a pattern used to identify gene identifiers, so they can be separated from TEs
+# Set a pattern used to identify gene identifiers, so they can be separated from TEs
 gene_id_regex_pattern = "ENS[A-Z]+[0-9]{11}"
 gene_id_regex = re.compile(gene_id_regex_pattern)
 footer_id_regex = re.compile("_[a-z]+")
 
-#Input identification and classification information for TE identifiers
-TE_df = pd.read_csv(args.TE_map_file,sep='\t')
+# Input identification and classification information for TE identifiers
+TE_df = pd.read_csv(args.TE_map_file, sep='\t')
 TE_mapping_dicts = dict()
-levels=['transcript_id','gene_id','family_id','class_id']
-for idx in range(len(levels)-1):
+levels = ['transcript_id', 'gene_id', 'family_id', 'class_id']
+for idx in range(len(levels) - 1):
     current_level = levels[idx]
-    next_level = levels[idx+1]
-    temp_TE_df = TE_df[[current_level,next_level]].drop_duplicates()
-    temp_TE_df.set_index(current_level,drop=True,inplace=True)
+    next_level = levels[idx + 1]
+    temp_TE_df = TE_df[[current_level, next_level]].drop_duplicates()
+    temp_TE_df.set_index(current_level, drop=True, inplace=True)
     TE_mapping_dicts[current_level] = temp_TE_df[next_level].to_dict()
 
 
-#Load input expression data
-eDF = pd.read_csv(args.input,sep='\t',index_col=0)
+# Load input expression data
+eDF = pd.read_csv(args.input, sep='\t', index_col=0)
 
-#Put gene data in one output file
+# Put gene data in one output file
 gene_ids = []
 footer_ids = []
 for x in eDF.index:
@@ -57,36 +57,38 @@ for x in eDF.index:
         gene_ids.append(x)
     if footer_id_regex.match(x):
         footer_ids.append(x)
-assert(len(footer_ids)<15)
+assert(len(footer_ids) < 15)
 
-eDF.loc[gene_ids+footer_ids,:].to_csv(args.output_prefix+'_gene_id_counts.tsv',sep='\t')
+eDF.loc[gene_ids + footer_ids,
+        :].to_csv(args.output_prefix + '_gene_id_counts.tsv', sep='\t')
 
-#Cut down matrix to just TE elements
-TE_ids = list(set(TE_df['transcript_id'])&set(eDF.index))
-eDF = eDF.loc[TE_ids+footer_ids,:]
+# Cut down matrix to just TE elements
+TE_ids = list(set(TE_df['transcript_id']) & set(eDF.index))
+eDF = eDF.loc[TE_ids + footer_ids, :]
 
-#Output to file
-output_filename = '{}_TE{}_counts.tsv'.format(args.output_prefix,'transcript_id')
-eDF.to_csv(output_filename,sep='\t')
+# Output to file
+output_filename = '{}_TE{}_counts.tsv'.format(
+    args.output_prefix, 'transcript_id')
+eDF.to_csv(output_filename, sep='\t')
 
-levels = ['gene_id','family_id','class_id'] #Must be in increasing level in heirarchy
-previous_level = 'transcript_id' #start with the most basic element
+# Must be in increasing level in heirarchy
+levels = ['gene_id', 'family_id', 'class_id']
+previous_level = 'transcript_id'  # start with the most basic element
 for level in levels:
     # Make an index for the TE dataframe at this level of description
     new_TE_index = list(set(TE_df[level])) + footer_ids
-    output_eDF = pd.DataFrame(0,columns=eDF.columns,index=new_TE_index)
-    output_eDF.loc[footer_ids,:] = eDF.loc[footer_ids,:]
+    output_eDF = pd.DataFrame(0, columns=eDF.columns, index=new_TE_index)
+    output_eDF.loc[footer_ids, :] = eDF.loc[footer_ids, :]
 
     # Each row from the lower level gets added to one row of the next level up
     for TE_id in eDF.index:
         if TE_id not in footer_ids:
             TE_id_to_add_to = TE_mapping_dicts[previous_level][TE_id]
-            output_eDF.loc[TE_id_to_add_to,:] += eDF.loc[TE_id,:]
+            output_eDF.loc[TE_id_to_add_to, :] += eDF.loc[TE_id, :]
 
-    output_filename = '{}_TE{}_counts.tsv'.format(args.output_prefix,level)
-    output_eDF.to_csv(output_filename,sep='\t')
+    output_filename = '{}_TE{}_counts.tsv'.format(args.output_prefix, level)
+    output_eDF.to_csv(output_filename, sep='\t')
     # Finish by resetting eDF to this output
     # This reduces the number of summations required
     eDF = output_eDF
     previous_level = level
-
